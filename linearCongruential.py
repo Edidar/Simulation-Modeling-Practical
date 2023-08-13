64c530cebd8a37e5d4c5af8e1c6e7a26e0e48364
Didar <edidar.cse@gmail.com>
2023-08-13 11:54:00 +0600

    LCM

--- linearCongruential.py
+++ linearCongruential.py
@@ -1,12 +1,12 @@
 x = int(input("Enter the value of initail Seed : "))
 tem =x
 a = int(input("Enter the value of a : "))
-c = int(input("Enter the value of c : "))
+c = int(input("Enter the value of c : ")) 
 m = int(input("Enter the value of m : "))
-
+#
 while 1:
     xi=(a*x+c)%m
-    Ri = xi/m
+    Ri = xi/m 
     x = xi
     if xi == tem or xi==0:
         break
