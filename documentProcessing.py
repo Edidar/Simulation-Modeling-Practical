b770a51357f9e71a17ca72667ae99d6384e99e11
Md.Didar Ahmed <106873568+Edidar@users.noreply.github.com>
2023-10-10 20:42:23 +0600

    Add files via upload

--- documentProcessing.py
+++ documentProcessing.py
@@ -1,43 +1,34 @@
-import random
 import pandas as pd
-n = int(input("Enter the Document Number :"))
-st = []
-wt = []
-ft = []
-cumT=[]
-bt = []
-nj = []
-nd=[]
-#print("Enter 1st starting time:")
 
-st.insert(0,0)
-#print("Enter 1st working  time:")
-wt.insert(0,45)
-ft.insert(0,st[0]+wt[0])
-cumT.insert(0,ft[0])
-bt.insert(0,0)
-nj.append(random.randint(51,59))
-nd.insert(0,0)
-for i in range (1,(n+1)):
-    n =random.randint(0,1)
-    bt.append(n)
-    nd.append(int(i))
-    l = random.randint(51,59)
-    nj.append(l)
-    workT=random.randint(5,45)
-    wt.append(workT)
-    if bt[int(i-1)]==1:
-        
-        st.append(ft[int(i-1)]+5)
-        ft.append(st[int(i)]+wt[int(i)])
-        cumT.append(wt[i])
-    else:
-        st.append(ft[int(i-1)])
-        ft.append(st[int(i)]+wt[int(i)])
-        cumT.append(st[int(i)]+wt[int(i)])
+st=[]
+wt=[45,16,5,29,33,25,21]
+ft =[]
+cm=[]
+bf=[]
+nj=[]
+s=c=0
+n=7
+noj=57
+#nj.append(noj)
+for i in range(0,n,1):
+    st.append(s)
+    s+=wt[i]
+    c+=wt[i]
     
+    nj.append(noj)
+    if(c>=60):
+        bf.append(1)
+        ft.append(s)
+        s+=5
+        cm.append(c)
+        c=0
+    else:
+        bf.append(0)
+        ft.append(s)
+        cm.append(c)
+    noj-=1
 
-dic={"Doument Number ":nd,"Start Time ": st ,"Work Time ":wt, "Finish Time":ft,"Cummulative Time":cumT,"Break Flag":bt,"Number of job":nj}
-
-show= pd.DataFrame(dic)
-print(show)
+print(nj,st,ft,bf,)
+dis ={"Start Time":st,"Work Time":wt,"Finish Time":ft,"Cummulative Time":cm,"Break":bf,"Number of Job":nj}
+r = pd.DataFrame(dis)
+print(r)
